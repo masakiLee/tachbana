78fdf0246d3e06168e82651c0a5093872ba0d2d3
masakiLee <everything2427@hotmail.com>
2023-02-08 12:50:25 +0800

    Five

--- src/App.vue
+++ src/App.vue
@@ -69,7 +69,7 @@
     <section class="concept">
       <div class="container">
         <div class="row">
-          <div class="col-md-4 pt-6 pt-md-8 pb-4 pb-md-8">
+          <div class="col-md-4 pt-6 pt-md-8 pb-0 pb-md-8">
             <div class="conceptSticky d-flex">
               <div class="conceptMain">
                 <h2 class="concept-title text-vertical">橘鮨理念</h2>
@@ -125,15 +125,24 @@
     <section class="menu">
       <div class="container-fluid">
         <div class="row">
-          <div class="col-lg-6 g-0 order-2 order-md-1">
+          <div class="col-xl-6 g-0 order-2 order-xl-1 d-none d-md-block">
             <ul class="menuImgBox d-flex flex-wrap">
               <li class="menuImgList">
+                <div class="menuHover d-flex align-items-center pt-3 pt-lg-5">
+                  <p>單品</p>
+                </div>
                 <img src="./assets/images/menuOne.jpg" alt="" class="menuImg" />
               </li>
               <li class="menuImgList">
+                <div class="menuHover d-flex align-items-center pt-3 pt-lg-5">
+                  <p>軍艦</p>
+                </div>
                 <img src="./assets/images/menuTwo.jpg" alt="" class="menuImg" />
               </li>
               <li class="menuImgList">
+                <div class="menuHover d-flex align-items-center pt-3 pt-lg-5">
+                  <p>套餐</p>
+                </div>
                 <img
                   src="./assets/images/menuThree.jpg"
                   alt=""
@@ -141,6 +150,9 @@
                 />
               </li>
               <li class="menuImgList">
+                <div class="menuHover d-flex align-items-center pt-3 pt-lg-5">
+                  <p>捲物</p>
+                </div>
                 <img
                   src="./assets/images/menuFour.jpg"
                   alt=""
@@ -150,12 +162,12 @@
             </ul>
           </div>
           <div
-            class="col-lg-6 g-0 border border-white border-2 bg-darkTwo d-flex flex-column justify-content-center order-1 order-md-2"
+            class="col-xl-6 border border-white border-2 bg-darkTwo d-flex flex-column justify-content-center order-1 order-xl-2"
           >
             <div class="menuMain py-6">
               <h2 class="menuTitle text-vertical mx-auto">美味菜單</h2>
               <h3 class="menuText text-center my-5">
-                美味的季節性魚類 融入創意理念 透過巧手變成創意壽司
+                橘鮨不僅提供美味的季節性魚類 融入創意理念 透過巧手變成創意壽司
                 在享受美食的同時體驗 別樹一格的餐饗
               </h3>
               <button
@@ -394,6 +406,9 @@
     padding: 240px 0;
     &:nth-child(odd) {
       margin-left: auto;
+      @media (max-width: 576px) {
+        padding: 0 0 24px 0;
+      }
     }
     @media (max-width: 768px) {
       width: 100%;
@@ -433,10 +448,47 @@
 .menu {
   &ImgList {
     width: 50%;
+    position: relative;
+    writing-mode: vertical-lr;
+    .menuHover {
+      position: absolute;
+      top: 50%;
+      left: 50%;
+      transform: translate(-50%, -50%);
+      height: 80%;
+      width: 80%;
+      border: 3px solid white;
+      z-index: 2;
+    }
+    @media (max-width: 1200px) {
+      width: 25%;
+    }
   }
+  &ImgList:nth-child(1) .menuHover {
+    background: url(./assets/images/menuHoverOne.png) no-repeat;
+    background-position: center 185%;
+    background-size: contain;
+  }
+  &ImgList:nth-child(2) .menuHover {
+    background: url(./assets/images/menuHoverTwo.png) no-repeat;
+    background-position: center 230%;
+    background-size: 80%;
+  }
+  &ImgList:nth-child(3) .menuHover {
+    background: url(./assets/images/menuHoverThree.png) no-repeat;
+    background-position: center 155%;
+    background-size: contain;
+  }
+  &ImgList:nth-child(4) .menuHover {
+    background: url(./assets/images/menuHoverFour.png) no-repeat;
+    background-position: center 180%;
+    background-size: contain;
+  }
+
   &Img {
     aspect-ratio: 1/1;
     object-fit: cover;
+    opacity: 0.3;
   }
   &Title {
     border: 3px solid #f25c05;
